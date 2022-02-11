6c7418695af4b30f9855eada699da420ce5e9d6c
Maksim <load.tpixel@gmail.com>
2022-02-11 16:37:46 +0200

    back | category up

--- code/server/controllers/Sidebar.js
+++ code/server/controllers/Sidebar.js
@@ -5,7 +5,7 @@ import parserUrl from '../helpers/parserUrl.js'
 
 
 class Controller {
-  
+
   async getSidebarData(req, res) {
     try {
       const parsedData = parserUrl(req)['filters']
@@ -14,9 +14,8 @@ class Controller {
 
       const tagIDs = await Tag.find((!searchSlugs) ? {} : { slug: searchSlugs }, { _id: true })
 
-
       const productTagIDs = await Product.find({ tagsID: { $in: tagIDs } }, { _id: false, tagsID: true })
- 
+
       const usedTagIDs =
         Object.values(
           productTagIDs.flat(1)
@@ -29,7 +28,7 @@ class Controller {
               return acc
             }, {})
         )
- 
+
       const usedTags = await Tag.find({ _id: usedTagIDs }, { __v: false });
 
       const usedTypesID =
@@ -46,28 +45,34 @@ class Controller {
 
       const usedTypes = await Type.find({ _id: usedTypesID }, { __v: false });
 
-      
-      //console.log(usedTags);
-      //console.log(usedTypes);
 
-      const filtersData = usedTags.reduce((acc, tag) => {
-        acc[tag.typeID] = {
-          ...acc[tag.typeID],
-          type: usedTypes.filter((type) => {
-            if (String(type._id) === String(tag.typeID)) {
-              return type
-            }
-          })[0],
-          tags: acc[tag.typeID] ? [...acc[tag.typeID]['tags'], tag] : [tag]
-          
-        } 
+      //UPPER CATEGORY
+      const filtersData = Object.values(usedTags
+        .reduce((acc, tag) => {
+          acc[tag.typeID] = {
+            ...acc[tag.typeID],
+            type: usedTypes.filter((type) => {
+              if (String(type._id) === String(tag.typeID)) {
+                return type
+              }
+            })[0],
+            tags: acc[tag.typeID] ? [...acc[tag.typeID]['tags'], tag] : [tag]
+          }
+          return acc
+        }, {}))
 
-        return acc
-      }, {})
+        
+        .reduce((acc, item) => {
+          if (item['type']['slug'] !== 'category') {
+            acc = [...acc, item]
+          } else {
+            acc = [item, ...acc]
+          }
+          return acc
+        }, [])
 
-      //console.log(filtersData);
 
-      res.status(200).json(Object.values(filtersData))
+      res.status(200).json(filtersData)
     } catch (error) {
       res.status(500).json(error)
     }
